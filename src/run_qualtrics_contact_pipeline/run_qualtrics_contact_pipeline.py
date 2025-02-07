fd1b42171278e7e8e461564f6655a5983cc796af
datasci-iopsy <datasci-iopsy@github.com>
2025-02-07 01:21:32 +0100

    nixed the airflow paradigm and updating gsheet using rowise functions for contacts. study variables should use gspread-dataframe paradigm for deeper cleaning

--- src/run_qualtrics_contact_pipeline/run_qualtrics_contact_pipeline.py
+++ src/run_qualtrics_contact_pipeline/run_qualtrics_contact_pipeline.py
@@ -1,19 +1,4 @@
-from scripts import process_contacts_proc
+from scripts import process_contacts_dict_ls
 
 # * run entry point
-process_contacts_proc.main()
-
-# # ! DEPRECATED: load sheet into pandas dataframe
-# raw_contacts_df = analytic_utils.load_sheet_to_df(worksheet=raw_contacts_sheet)
-# print(f"Raw df:\n {raw_contacts_df}\n")
-# # print(raw_contacts_df.shape)
-
-# # ! DEPRECATED transformation magic
-# clean_contacts_df = analytic_utils.transform_df(
-#     df=raw_contacts_df,
-#     rename_map=config.analytic_params.rename_map,
-#     bool_key=config.analytic_params.boolean_key,
-#     bool_value=config.analytic_params.boolean_value,
-# )
-# print(f"Clean df:\n {clean_contacts_df}\n")
-# # print(clean_contacts_df.shape)
+process_contacts_dict_ls.main()
