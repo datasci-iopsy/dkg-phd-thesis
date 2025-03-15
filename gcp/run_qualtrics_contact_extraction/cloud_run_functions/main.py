51fea1cc66d91f990bc02b6c70308268b1d1df52
Demetrius K. Green <datasci-iopsy@github.com>
2025-03-15 07:00:26 +0100

    dev branch work to try gsheet to bq process since authorization is more controlled

--- gcp/run_qualtrics_contact_extraction/cloud_run_functions/main.py
+++ gcp/run_qualtrics_contact_extraction/cloud_run_functions/main.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from google.cloud import bigquery
 from pathlib import Path
 
-from utils import common_utils
+from utils import common_utils, gcp_utils
 
 # dynamically resolve the path to configs directory relative to this script
 py_file = Path(__file__).resolve()  # path of this file (main.py)
@@ -17,15 +17,24 @@ print(f"Resolved config path: {config_path}")
 
 # load configuration using resolved path
 config = common_utils.load_configs(config_path=str(config_path), use_box=True)
-print(f"Loaded config: {json.dumps(config, indent=4)}")
-
-# define required fields from config
-REQUIRED_FIELDS = config.params.bq.raw_table_fields
-# REQUIRED_FIELDS = config.get("params", {}).get("bq", {}).get("raw_table_fields", [])
+print(f"Loaded config: {json.dumps(config, indent=2)}")
 
 
 @functions_framework.http
-def qualtrics_receiver(request):
+def qualtrics_gsheet_db_handler(request):
+    # verify API key
+    EXPECTED_API_KEY = gcp_utils.get_secret_payload(
+        project_id=config.params.gcp.project_id,
+        secret_id=config.params.secret_mgr.api_key_secret_id,
+        version_id=config.params.secret_mgr.version_id,
+    )
+    print(EXPECTED_API_KEY)
+
+    api_key = request.headers.get("X-API-KEY")
+    if api_key != EXPECTED_API_KEY.get("key"):
+        print(f"Received API key: {api_key}")
+        return jsonify({"error": "Unauthorized"}), 401
+
     # check if the request is JSON
     if not request.is_json:
         return jsonify(
@@ -40,6 +49,8 @@ def qualtrics_receiver(request):
         return jsonify({"error": f"Invalid JSON payload: {str(e)}"}), 400
 
     # validate required keys in payload
+    REQUIRED_FIELDS = config.params.bq.raw_table_fields
+    # REQUIRED_FIELDS = config.get("params", {}).get("bq", {}).get("raw_table_fields", [])
     missing_keys = [key for key in REQUIRED_FIELDS if key not in payload]
     if missing_keys:
         return jsonify({"error": f"Missing required fields: {missing_keys}"}), 400
@@ -65,4 +76,8 @@ def qualtrics_receiver(request):
     except Exception as e:
         return jsonify({"error": f"BigQuery insertion failed: {str(e)}"}), 500
 
-    return jsonify({"status": "success"}), 200
+    return jsonify(
+        {
+            "status": f"gsheets data inserted into {project_id}.{dataset_id}.{table_id} BigQuery successfully"
+        }
+    ), 200
--- gcp/scratchpad.sh
+++
@@ -1,5 +0,0 @@
-functions-framework \
-    --source='run_qualtrics_contact_extraction/cloud_run_functions/main.py' \
-    --target=qualtrics_receiver \
-    --host=localhost \
-    --port=8080
