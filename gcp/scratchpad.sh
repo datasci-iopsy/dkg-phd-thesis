4b490f2f04a64ce4e88bcc5aae4141b49458785a
Demetrius K. Green <datasci-iopsy@github.com>
2025-03-18 02:23:21 +0100

    dev is in a good state and it seems to work locally but not from deployed cloud

--- gcp/scratchpad.sh
+++
@@ -1,5 +0,0 @@
-functions-framework \
-    --source='run_qualtrics_contact_extraction/cloud_run_functions/main.py' \
-    --target=qualtrics_gsheet_db_handler \
-    --host=localhost \
-    --port=8080
