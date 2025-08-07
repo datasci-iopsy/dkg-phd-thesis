578e47ae3690d27603af5bab8664ebeb629cda96
datasci-iopsy <14611107+datasci-iopsy@users.noreply.github.com>
2025-08-07 02:03:44 +0200

    tinkered with visual script and in good place; now need to build out process and use ggpubr for plots

--- srcR/run_power_reporting/utils/power_visual_utils.r
+++ srcR/run_power_reporting/utils/power_visual_utils.r
@@ -7,26 +7,48 @@ library(dplyr) # masks stats::filter, lag; base::intersect, setdiff, setequal, u
 library(readr)
 library(skimr)
 
-power_results <- readr::read_csv(
+sim_odds <- readr::read_csv(
     file = here::here("srcR/run_power_analysis/data/power_analysis_results_20250801_123346.csv")
 )
+
+sim_evens <- readr::read_csv(
+    file = here::here("srcR/run_power_analysis/data/power_analysis_results_20250805_143050.csv")
+)
+
+# combine the two datasets
+power_results <- dplyr::bind_rows(sim_odds, sim_evens)
 dplyr::glimpse(power_results)
 skimr::skim(power_results)
 
 
+l1_eff <- 0.1
+l2_eff <- 0.30
+x_eff <- 0.30
+icc <- 0.30
+rs_eff <- 0.09
+
 # plot power curves
 power_results |>
     dplyr::filter(
-        Level1_Effect_Std == 0.30 &
-            Level2_Effect_Std == 0.30 &
-            XLevel_Intxn_Effect_Std == 0.30 &
-            ICC == 0.30 &
-            Random_Slope_Std == 0.09
+        Level1_Effect_Std == l1_eff &
+            Level2_Effect_Std == l2_eff &
+            XLevel_Intxn_Effect_Std == x_eff &
+            ICC == icc &
+            Random_Slope_Std == rs_eff
     ) |>
     ggplot2::ggplot(aes(x = N_Level2, y = Power, color = Effect)) +
     geom_line(linewidth = 1) +
+    # geom_ribbon(
+    #     aes(ymin = Lower_CI, ymax = Upper_CI, fill = Effect),
+    #     alpha = 0.2,
+    #     color = NA
+    # ) +
+    geom_errorbar(aes(ymin = Lower_CI, ymax = Upper_CI),
+        width = 20, alpha = 0.6
+    ) +
     geom_point(size = 2) +
-    geom_hline(yintercept = 0.8, linetype = "dashed", alpha = 0.5) +
+    geom_hline(yintercept = 0.80, linetype = "dashed", alpha = 0.5) +
+    geom_hline(yintercept = 0.90, linetype = "dashed", alpha = 0.5) +
     labs(
         title = "Power Analysis: Sample Size Effects",
         x = "Level 2 Sample Size (n_lvl2)",
@@ -39,4 +61,3 @@ power_results |>
         limits = c(0, max(power_results$N_Level2)),
         breaks = seq(min(power_results$N_Level2), max(power_results$N_Level2), by = 100)
     )
-# facet_wrap(~ Level2_Effect_Std + XLevel_Intxn_Effect_Std, nrow = 3, ncol = 3)
