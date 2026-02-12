cb01805d13b14efd3230b0cf98d63f1bb7bbf1a4
datasci-iopsy <14611107+datasci-iopsy@users.noreply.github.com>
2026-02-12 02:27:44 +0100

    gcp files before refactoring entire repo

--- gcp/scripts/deploy_run_qualtrics_scheduling.sh
+++ gcp/scripts/deploy_run_qualtrics_scheduling.sh
@@ -6,11 +6,12 @@ cp -r ../shared/ ../cloud_run_functions/run_qualtrics_scheduling/shared/
 gcloud functions deploy run-qualtrics-scheduling \
         --gen2 \
         --runtime=python312 \
-        --region=us-central1 \
-        --source=functions/qualtrics_ingestion \
-        --entry-point=main \
+        --region=us-east4 \
+        --source=../cloud_run_functions/run_qualtrics_scheduling \
+        --entry-point=qualtrics_webhook_handler \
         --trigger-http \
-        --set-secrets=QUALTRICS_API_KEY=dkg-qualtrics-api-key:latest,QUALTRICS_WEBHOOK_SECRET=dkg-qualtrics-webhook-secret:latest
+        --set-secrets=QUALTRICS_API_KEY=dkg-qualtrics-api-key:latest,QUALTRICS_WEBHOOK_SECRET=dkg-qualtrics-webhook-secret:latest \
+        --allow-unauthenticated
 
-# Clean up copied files
-rm -rf functions/qualtrics_ingestion/shared/
+# # Clean up copied files
+# rm -rf functions/qualtrics_ingestion/shared/
